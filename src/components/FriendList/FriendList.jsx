e5f089cce7cac5484220ed4279e5f8658a3af650
ievsepl <107007762+ievsepl@users.noreply.github.com>
2022-12-21 19:03:40 +0200

    add ch1

--- src/components/FriendList/FriendList.jsx
+++ src/components/FriendList/FriendList.jsx
@@ -1,6 +1,6 @@
 import css from './FriendList.module.css'
 
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 import { FriendListItem } from '../FriendListItem/FriendListItem'
 
 export const FriendList = ({ friends }) => <ul className={css.friendList}>
